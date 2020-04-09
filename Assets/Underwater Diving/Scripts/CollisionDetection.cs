f56a15bc06caaeb9dd9bdc4ea097e2e0034a26f6
dannygoodrich <dannyngoodrich@gmail.com>
2020-04-08 17:43:43 -0700

    score

---
+++ Assets/Underwater Diving/Scripts/CollisionDetection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    void OnCollisionEnter2D(UnityEngine.Collision2D collisionInfo)
+    {
+        Debug.Log(transform.localScale.x);
+        if (transform.localScale.x < 0)
+        {
+            transform.localScale += new Vector3(-.2f, .2f);
+            Destroy(collisionInfo.gameObject);
+            Debug.Log(collisionInfo.collider);
+            Debug.Log(collisionInfo);
+
+        }
+        else if (transform.localScale.x > 0)
+        {
+            transform.localScale += new Vector3(.2f, .2f);
+            Destroy(collisionInfo.gameObject);
+        }
+        //transform.localScale += new Vector3(Math.Abs(1.01f), 1.01f);
+        //transform.localScale += new Vector3(1.01f, 1.01f);
+        Debug.Log("hit something!");
+    }
+}
